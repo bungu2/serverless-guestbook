b3e905ebe7cedf5d9a0743ff8e87c5ae4c904e31
bungu2 <bungu2@yahoo.com>
2021-02-26 16:00:31 +0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://eb1297eb.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://a444a8f7.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
